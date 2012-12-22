0c4fafe545ec2cf187c5de3a005393e741c59be3
Eric <eric@Thor-2.local>
2012-12-22 15:34:02 +0100

    dbinstall fixes

--- dbinstall.php
+++ dbinstall.php
@@ -1,5 +1,7 @@
 <?php
-    $con = mysql_connect("localhost","peter","abc123"); if (!$con)
+    $con = mysql_connect("localhost","root",""); 
+    
+    if (!$con)
 	{
 		die('Could not connect: ' . mysql_error()); 
 	}
@@ -7,73 +9,108 @@
 	
 	
 	// Create table 
-	mysql_select_db("my_db", $con); 
+	mysql_select_db("seapal", $con); 
 	
-	$sql = "CREATE TABLE Boat 	(	Boatname varchar(255), 
-									Registernr varchar(15),
-									PRIMARY KEY(Registernr), 
-									Sailemblem varchar(20), 
-									Homeport varchar(30), 
-									Yachtclub varchar(20), 
-									Owner varchar(255), 
-									Insurance 
-									varchar(100),
-									Callsign varchar(50),
-									Type varchar(15),
-									Constructor varchar(50),
-									Length double,
-									Width double,
-									Draft double,
-									Mastheight double,
-									Repression double,
-									Rigtype varchar(15),
-									Manufactureyear int,
-									Motortype varchar(50),
-									Tanksize double,
-									Watertanksize double,
-									Effluenttanksize double,
-									Mainsailsize double,
-									Genuasize double,
-									Spisize double
-									);
+	$sql = "CREATE TABLE IF NOT EXISTS Boat 			(	Boatname varchar(255), 
+															Registernr varchar(15),
+															PRIMARY KEY(Registernr), 
+															Sailemblem varchar(20), 
+															Homeport varchar(30), 
+															Yachtclub varchar(20), 
+															Owner varchar(255), 
+															Insurance 
+															varchar(100),
+															Callsign varchar(50),
+															Type varchar(15),
+															Constructor varchar(50),
+															Length double,
+															Width double,
+															Draft double,
+															Mastheight double,
+															Repression double,
+															Rigtype varchar(15),
+															Manufactureyear int,
+															Motortype varchar(50),
+															Tanksize double,
+															Watertanksize double,
+															Effluenttanksize double,
+															Mainsailsize double,
+															Genuasize double,
+															Spisize double
+															);
+															
+															
 									
-			CREATE TABLE Trip	(	TripID int NOT NULL AUTO_INCREMENT,
-									PRIMARY KEY(TripID),
-									Triptitle varchar(255), 
-									From varchar(100), 
-									To varchar(100), 
-									Skipper varchar(30), 
-									Start datetime, 
-									End datetime, 
-									Duration int,
-									Motor int,
-									Tankfilled BOOL,
-									Notes TEXT,
-									Picture blob
-									);
+			CREATE TABLE IF NOT EXISTS Trip				(	id INT NOT NULL AUTO_INCREMENT,
+															Triptitle varchar(255), 
+															FromLocation varchar(100), 
+															ToLocation varchar(100), 
+															Skipper varchar(30), 
+															StartDate DATETIME, 
+															EndDate DATETIME, 
+															Duration int,
+															Motor int,
+															Tankfilled BOOL,
+															Notes TEXT,
+															Picture blob,
+															PRIMARY KEY(id)
+															);
 									
-			CREATE TABLE Waypoint (	WaypointID int NOT NULL AUTO_INCREMENT,
-									PRIMARY KEY(WaypointID),
-									Name varchar(255), 
-									BTM double, 
-									DTM double, 
-									COG double, 
-									SOG double, 
-									Date datetime, 
-									Tripto int,
-									Maneuver int,
-									Mainsail int,
-									Grandsail int
-									);
-			";
+			
+									
+			CREATE TABLE IF NOT EXISTS Waypoint 		(	id int NOT NULL AUTO_INCREMENT,
+															PRIMARY KEY(id),
+															Name varchar(255), 
+															PositionEdegree int,
+															PositionEmin int,
+															PositionEsek int,
+															PositionNdegree int,
+															PositionNmin int,
+															PositionNsek int,
+															BTM double, 
+															DTM double, 
+															COG double, 
+															SOG double, 
+															Date DATETIME, 
+															Tripto int,
+															Maneuver int,
+															Mainsail int,
+															Grandsail int
+															);
+									
+			CREATE TABLE IF NOT EXISTS WaypointOfTrip	(	WaypointID int,
+															TripID int);
+			
+			ALTER TABLE Trip AUTO_INCREMENT=10000;
+			ALTER TABLE Waypoint AUTO_INCREMENT=10000;";
+	
+
+	$queries = explode(";", $sql);
 	
 	// Execute query
-	mysql_query($sql,$con);
+	
+	foreach ($queries as $query) {
+			
+			if (strlen($query) == 0) {
+				continue;
+			}
+			
+			$result = mysql_query($query,$con);
+			
+			if ($result == true) {
+				echo "Table installed!<br />";
+			} else {
+		
+				echo "Install Table Failed! Error: ".mysql_error()."<br />";	
+		
+			}
+	}
 	
 	
 	
 	
 	
 	
-	  mysql_close($con);
+	
 ?>
+
